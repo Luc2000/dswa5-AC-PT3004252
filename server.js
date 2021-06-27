abe71f8f41d67521a593e2ced83be03f702d9726
Lucas Souza <lucas.annunziato@aluno.ifsp.edu.br>
2021-06-27 15:48:06 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var app = require('./config/express')();
+
+const url = 
+'mongodb+srv://lucas:root@cluster0.vr2ft.mongodb.net/IFSP?retryWrites=true&w=majority';
+require('./config/database.js')(url);
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express Server escutando na porta ' + app.get('port'));
+});
